26cc08fdfc3fe888993ac75f6bfbcadf4346e718
mathmaths <44037705+mathmaths@users.noreply.github.com>
2019-04-01 15:36:12 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -8,11 +8,14 @@
 
 'use strict';
 
+const request = require('request');
+
 // Imports dependencies and set up http server
 const
   express = require('express'),
   bodyParser = require('body-parser'),
   app = express().use(bodyParser.json()); // creates express http server
+
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
